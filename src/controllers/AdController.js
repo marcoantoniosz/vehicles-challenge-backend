f0c98fb579109ea582955db75ace81b110519fd0
Marco <marcoantonio99d@gmail.com>
2022-07-11 12:43:36 -0300

    Done

--- src/controllers/AdController.js
+++ src/controllers/AdController.js
@@ -8,15 +8,15 @@ router.get('/ads/filters', Mw.validateFilters, Mw.notFoundFilters, Mw.getFilters
 
 router.get('/ads/search', Mw.validateQuery, Mw.notFoundQuery, Mw.getQuerry);
 
-router.get('/ads/:id', Mw.validateId, Mw.notFoundId, Mw.getById);
-
 router.get('/ads/favorite', Mw.notFoundFavorite, Mw.getFavorite);
+
+router.get('/ads/:id', Mw.validateId, Mw.notFoundId, Mw.getById);
 
 router.post('/ads', Mw.validateInputs, Mw.createAd);
 
-router.put('/ads/:id', Mw.validateInputs, Mw.updateAd);
-
 router.put('/ads/favorite/:id', Mw.updateFavorite);
+
+router.put('/ads/:id', Mw.validateInputs, Mw.updateAd);
 
 router.delete('/ads/:id', Mw.deleteAd);
 
