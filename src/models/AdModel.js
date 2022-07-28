2b3921c60b6a44b8f5782463e963d19779d93ef9
Marco <marcoantonio99d@gmail.com>
2022-07-28 13:05:33 -0300

    DB

--- src/models/AdModel.js
+++ src/models/AdModel.js
@@ -1,22 +1,24 @@
+require('dotenv').config();
 const connection = require('./connection');
+const DB = process.env.SQL_DATABASE;
 
 const getAll = async () => {
 	const [ads] = await connection.execute(
-		'SELECT * FROM sql10504076.ad;',
+		`SELECT * FROM ${DB};`,
 	);
 	return ads;
 };
 
 const getById = async (id) => {
 	const [ads] = await connection.execute(
-		`SELECT * FROM sql10504076.ad WHERE ad_id = ${id};`,
+		`SELECT * FROM ${DB} WHERE ad_id = ${id};`,
 	);
 	return ads;
 };
 
 const getByQuery = async (query) => {
 	const [ads] = await connection.execute(
-		`SELECT * FROM sql10504076.ad WHERE car_name LIKE '%${query}%' OR car_brand LIKE '%${query}%' OR car_color LIKE '%${query}%' OR car_year LIKE '%${query}%' OR car_plate LIKE '%${query}%' OR car_description LIKE '%${query}%' OR car_price LIKE '%${query}%';`,
+		`SELECT * FROM ${DB} WHERE car_name LIKE '%${query}%' OR car_brand LIKE '%${query}%' OR car_color LIKE '%${query}%' OR car_year LIKE '%${query}%' OR car_plate LIKE '%${query}%' OR car_description LIKE '%${query}%' OR car_price LIKE '%${query}%';`,
 	);
 	return ads;
 };
@@ -24,7 +26,7 @@ const getByQuery = async (query) => {
 
 const getByFilters = async (b, c, y, min, max) => {
 	const [ads] = await connection.execute(
-		`SELECT * FROM sql10504076.ad WHERE car_brand LIKE '%${b}%' AND car_color LIKE '%${c}%' AND car_year LIKE '%${y}%' AND car_price BETWEEN '${min}' AND '${max}';`,
+		`SELECT * FROM ${DB} WHERE car_brand LIKE '%${b}%' AND car_color LIKE '%${c}%' AND car_year LIKE '%${y}%' AND car_price BETWEEN '${min}' AND '${max}';`,
 	);
 	return ads;
 };
@@ -32,14 +34,14 @@ const getByFilters = async (b, c, y, min, max) => {
 
 const getByFavoriteStatus = async (favorite) => {
 	const [ads] = await connection.execute(
-		`SELECT * FROM sql10504076.ad WHERE favorite = ${favorite};`,
+		`SELECT * FROM ${DB} WHERE favorite = ${favorite};`,
 	);
 	return ads;
 };
 
 const createNewAd = async (newAd) => {
 	const [result] = await connection.execute(
-		'INSERT INTO sql10504076.ad (car_name, car_brand, car_color, car_year, car_plate, car_description, car_price, favorite) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+		`INSERT INTO ${DB} (car_name, car_brand, car_color, car_year, car_plate, car_description, car_price, favorite) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
 		[newAd.name, newAd.brand, newAd.color, newAd.year, newAd.plate, newAd.description, newAd.price, newAd.favorite],
 	);
 	return result;
@@ -47,7 +49,7 @@ const createNewAd = async (newAd) => {
 
 const updateAd = async (ad, id) => {
 	const [result] = await connection.execute(
-		'UPDATE sql10504076.ad SET car_name = ?, car_brand = ?, car_color = ?, car_year = ?, car_plate = ?, car_description = ?, car_price = ?, favorite = ? WHERE ad_id = ?',
+		`UPDATE ${DB} SET car_name = ?, car_brand = ?, car_color = ?, car_year = ?, car_plate = ?, car_description = ?, car_price = ?, favorite = ? WHERE ad_id = ?`,
 		[ad.name, ad.brand, ad.color, ad.year, ad.plate, ad.description, ad.price, ad.favorite, id],
 	);
 	return result;
@@ -55,7 +57,7 @@ const updateAd = async (ad, id) => {
 
 const updateFavorite = async (favorite, id) => {
 	const [result] = await connection.execute(
-		'UPDATE sql10504076.ad SET favorite = ? WHERE ad_id = ?',
+		`UPDATE ${DB} SET favorite = ? WHERE ad_id = ?`,
 		[favorite, id],
 	);
 	return result;
@@ -63,7 +65,7 @@ const updateFavorite = async (favorite, id) => {
 
 const deleteAd = async (id) => {
 	const [result] = await connection.execute(
-		'DELETE FROM sql10504076.ad WHERE ad_id = ?',
+		`DELETE FROM ${DB} WHERE ad_id = ?`,
 		[id],
 	);
 	return result;
